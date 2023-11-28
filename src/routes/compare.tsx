df8e77ebf44ab791004a7b6aab0830c5d83af5b5
acstanton515 <ec2-user@ip-10-0-0-192.ec2.internal>
2023-11-28 04:12:14 +0000

    updates to the branch and added inputs for civ and age

--- src/routes/compare.tsx
+++ src/routes/compare.tsx
@@ -1,25 +1,96 @@
-import { createMemo, createResource, Show, Suspense } from "solid-js";
+import { Link, NavLink, useIsRouting, useLocation } from "@solidjs/router";
+import { Component, createMemo, createResource, createSignal, Show, Suspense, onCleanup, For } from "solid-js";
 import { StatBar } from "../components/Stats";
-import { globalAgeFilter } from "../components/Toolbar";
+import { globalAgeFilter, hideNav, setHideNav } from "../global";
 import { UnitCard } from "../components/UnitCard";
 import { CIVILIZATIONS, ITEMS } from "../config";
-import { fetchItem } from "../query/fetch";
+import { findClosestMatch } from "../query/utils";
 import { calculateStatParts, getUnitStats } from "../query/stats";
 import { modifierMatches } from "../query/utils";
 import { civAbbr, Item, Modifier, Unit } from "../types/data";
 import { CalculatedStats, Stat, StatProperty } from "../types/stats";
+import { CivFlag } from "../components/CivFlag";
+import { Icon } from "../components/Icon";
 
-async function getComparer(unit: string, civAbbr: civAbbr) {
-  const civ = CIVILIZATIONS[civAbbr];
-  const item = await fetchItem(ITEMS.UNITS, unit);
-  const stats = await getUnitStats(ITEMS.UNITS, item, civ);
+async function getComparer(unit: { name: string, civAbbr: civAbbr }) {
+  const civ = CIVILIZATIONS[unit.civAbbr];
+  const item = await findClosestMatch(ITEMS.UNITS, unit.name, civ);
+  const stats = await getUnitStats(ITEMS.UNITS, unit.name, civ);
   return { item, stats, civ };
-}
+};
+
+
+const CompareToolbar: Component<{ player: string, ageFilter: any, setAgeFilter: any, setCivFilter: any}> = (props) => { //civFilter, setCivFilter 
+  const pending = useIsRouting();
+  const navButtonClass =
+    "w-12 h-10 md:w-10 lg:h-8 md:hover:bg-white md:hover:text-black bg-gray-900 text-white/70text-lg px-3 grid rounded-md flex-none transition";
+
+  return (
+    <div class="bg-gray-700 z-10 border-bottom border border-gray-500 sticky mt-25 top-0" classList={{ "opacity-20": pending() }}>
+      <div class="max-w-screen-2xl py-2 px-4 lg:px-8 h-auto text-base lg:p-3 mx-auto flex flex-row items-center flex-wrap sm:flex-nowrap gap-2 lg:gap-5">
+        <div class="hidden lg:flex flex-row gap-2 h-10 lg:h-8 items-center">
+          <h2>{props.player}</h2>
+          <For each={Object.values(CIVILIZATIONS)}>
+            {(civ) => (
+              <div class="group inline-block">
+              <button 
+                class="relative w-12 h-7 rounded-md overflow-hidden border-2 shadow-inner opacity-50 hover:opacity-100  border-transparent hidden xl:block transition"
+                onClick={() => props.setCivFilter(civ.abbr)}
+              >
+                <CivFlag abbr={civ.abbr} class="h-full w-full object-cover" />
+              </button>
+              </div>
+            )}
+          </For>
+        </div>
+        <div class="flex items-center text-center bg-gray-900 h-10 lg:h-8 rounded-md">
+          <button
+            class="w-8 h-full z-2 -mr-4 disabled:opacity-50"
+            onClick={() => props.setAgeFilter(Math.max(1, props.ageFilter() - 1))}
+            disabled={props.ageFilter() == 1}
+          >
+            <Icon icon="angle-left" />
+          </button>
+          <span class="w-12 md:w-24 pointer-events-none">
+            <span class="hidden md:inline">Age </span>
+            {["I", "II", "III", "IV"][props.ageFilter() - 1]}
+          </span>
+          <button
+            class="w-8 h-full z-2 -ml-4 disabled:opacity-50"
+            onClick={() => props.setAgeFilter(Math.min(4, props.ageFilter() + 1))}
+            disabled={props.ageFilter() == 4}
+          >
+            <Icon icon="angle-right" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export const CompareRoute = () => {
-  const [ally] = createResource(() => getComparer("spearman", "en"));
+  setHideNav(true);
+  onCleanup(() => setHideNav(false));
+  
+  const [show, setShow] = createSignal(false);
+  
+  const [allyAgeFilter, setAllyAgeFilter] = createSignal(4);
+  
+  const [allyUnitFilter, setAllyUnitFilter] = createSignal<string>('spearman');
+  const [allyCivFilter, setAllyCivFilter] = createSignal<civAbbr>('ab');
+  //const [ally] = createResource(() => getComparer("spearman", allyCivFilter);
+  const derivedAllyUnit = createMemo(() => ({ name: allyUnitFilter(), civAbbr: allyCivFilter() }));
+  const [ally] = createResource(derivedAllyUnit, getComparer);
 
-  const [enemy] = createResource(() => getComparer("royal-knight", "fr"));
+
+  const [enemyAgeFilter, setEnemyAgeFilter] = createSignal(4);
+  
+  const [enemyUnitFilter, setEnemyUnitFilter] = createSignal<string>('archer');
+  const [enemyCivFilter, setEnemyCivFilter] = createSignal<civAbbr>('de');
+  //const [enemy] = createResource(() => getComparer("horseman", enemyCivFilter);
+  const derivedEnemyUnit = createMemo(() => ({ name: enemyUnitFilter(), civAbbr: enemyCivFilter() }));
+  const [enemy] = createResource(derivedEnemyUnit, getComparer);
 
   const compareProps: Partial<StatProperty[]> = [
     "hitpoints",
@@ -34,8 +105,8 @@ export const CompareRoute = () => {
   ];
   const netstats = createMemo(() =>
     compareProps.reduce((acc, prop) => {
-      const att = calculateStatParts(ally()?.stats[prop], globalAgeFilter(), { target: enemy()?.item, decimals: 2 });
-      const def = calculateStatParts(enemy()?.stats[prop], globalAgeFilter(), { target: ally()?.item, decimals: 2 });
+      const att = calculateStatParts(ally()?.stats[prop], allyAgeFilter(), { target: enemy()?.item, decimals: 2 });
+      const def = calculateStatParts(enemy()?.stats[prop], enemyAgeFilter(), { target: ally()?.item, decimals: 2 });
 
       acc[prop] = { diff: att.total + att.bonus - def.total + def.bonus, ally: att, enemy: def };
       return acc;
@@ -61,8 +132,8 @@ export const CompareRoute = () => {
 
     const allyAttacksNeed = Math.ceil(netstats().hitpoints.enemy.max / allyDamage),
       enemyAttacksNeed = Math.ceil(netstats().hitpoints.ally.max / enemyDamage),
-      allyAttackSpeed = netstats().attackSpeed.enemy.max,
-      enemyAttackSpeed = netstats().attackSpeed.ally.max,
+      allyAttackSpeed = netstats().attackSpeed.ally.max,
+      enemyAttackSpeed = netstats().attackSpeed.enemy.max,
       allyTimeNeed = allyAttacksNeed * allyAttackSpeed,
       enemyTimeNeed = enemyAttacksNeed * enemyAttackSpeed,
       allyHealthLeft = Math.max(0, netstats().hitpoints.ally.max - Math.floor(allyTimeNeed / enemyAttackSpeed) * enemyDamage),
@@ -89,9 +160,12 @@ export const CompareRoute = () => {
       winner: winner,
     };
   });
-
+  
   return (
     <>
+      <CompareToolbar player="You  " ageFilter={allyAgeFilter} setAgeFilter={setAllyAgeFilter} setCivFilter={setAllyCivFilter}></CompareToolbar>
+      <CompareToolbar player="Enemy" ageFilter={enemyAgeFilter} setAgeFilter={setEnemyAgeFilter} setCivFilter={setEnemyCivFilter}></CompareToolbar>
+    
       <div class="max-w-screen-lg p-4 mx-auto">
         <div class="flex gap-4">
           <Show when={!ally.loading && !enemy.loading && results()}>
@@ -101,6 +175,10 @@ export const CompareRoute = () => {
                 {results().ally.attacksNeeded} attacks and {results().ally.timeNeeded} seconds to kill {enemy().item.name} and has {results().ally.hpLeft} HP
                 left.
               </p>
+              <div>
+                <pre>{JSON.stringify(results().ally, null, 2)}</pre>
+                <pre>{JSON.stringify(results().ally, null, 2)}</pre>
+              </div>
               {netstats().moveSpeed.ally.max > netstats().moveSpeed.enemy.max && <p>+ Can outrun/kite {enemy().item.name}</p>}
               <UnitCard unit={ally().item} civ={ally().civ}></UnitCard>
             </div>
@@ -114,6 +192,11 @@ export const CompareRoute = () => {
                 needs {results().enemy.attacksNeeded} attacks and {results().enemy.timeNeeded} seconds to kill {ally().item.name} and has{" "}
                 {results().enemy.hpLeft} HP left.
               </p>
+              <p>{}</p>
+              <div>
+                <pre>{JSON.stringify(results().ally, null, 2)}</pre>
+                <pre>{JSON.stringify(results().enemy, null, 2)}</pre>
+              </div>
               {netstats().moveSpeed.enemy.max > netstats().moveSpeed.ally.max && <p>+ Can outrun/kite {ally().item.name}</p>}
               <UnitCard unit={enemy().item} civ={enemy().civ}></UnitCard>
             </div>
